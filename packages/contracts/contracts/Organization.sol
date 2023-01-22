f025ba6e543f8191905f52a483e085671c97c60e
hector <hmgarcia06@gmail.com>
2023-01-22 00:38:03 -0500

    Added console.log for data.length

--- packages/contracts/contracts/Organization.sol
+++ packages/contracts/contracts/Organization.sol
@@ -84,6 +84,7 @@ contract Organization {
         // Call returns a boolean value indicating success or failure.
         // This is the current recommended method to use.
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        console.log("data.length: %s", data.length);
         require(sent, "Failed to send Ether");
     }
 
