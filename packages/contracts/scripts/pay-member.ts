5cb0f37e9428af587e366dd746e91c6c118c2772
hector <hmgarcia06@gmail.com>
2023-01-22 06:51:59 -0500

    Added paymembers script

--- packages/contracts/scripts/pay-member.ts
+++ packages/contracts/scripts/pay-member.ts
@@ -1,20 +1,20 @@
 import { ethers } from "hardhat";
-import { Contract } from "ethers";
-import { PAYROll } from "../utils/constants";
+import { Contract, ContractInterface } from "ethers";
+import { MEMBERS } from "../utils/constants";
 
 // NOTE: This is the address of the deployed contract
 // NOTE: You can get this address from the console after running the deploy script
 // TODO: Add a way to get the address through a written file after running the deploy script.
 const organizationAddress = "0x3Af846E763872a0bC8B4977Be9Ee0aca957F8884";
-// NOTE: Be sure that members variable is are members of the organization 
-// else the transaction will fail.
-
+// NOTE: Be sure that member variable is a member of the organization else the transaction will fail.
+const member = MEMBERS[0];
+const amount = ethers.utils.parseEther("0.000001234567");
 async function main() {
-    console.log("Paying members...");
+    console.log("Paying member...");
     const OrganizationContract = await ethers.getContractAt('Organization', organizationAddress);
-    const tx = await OrganizationContract.payMembers(PAYROll.MEMBERS, PAYROll.PAYMENTS);
+    const tx = await OrganizationContract.payMember(member, amount);
     const txResult = await tx.wait();
-    console.log('payMembers() txResult', txResult);
+    console.log('payMember() txResult', txResult);
 }
 
 main().catch((error) => {
