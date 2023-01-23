b2b3be39e37ad4e5253ce90ab14fe8fd124258c9
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Replaced hardcoded contract info with constants variables in deploy script

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,15 +1,25 @@
 import { ethers } from "hardhat";
 
-import { MEMBERS } from "../utils/constants";
+import { MEMBERS, HARDHAT_NETWORK, ORGANIZATION_NAME, ORGANIZATION_CONTRACT, ORGANIZATION_DEPOSIT, ORGANIZATION_CONTRACT_FILE_PATH } from "../utils/constants";
+import { writeJSONFile } from "../utils/files";
 
 async function main() {
-  const depositAmount = ethers.utils.parseEther("0.00016190");
-
-  const Organization = await ethers.getContractFactory("Organization");
-  const organization = await Organization.deploy("MyOrganizationDeployed", MEMBERS.slice(0, MEMBERS.length -1), { value: depositAmount});
+  const depositAmount = ORGANIZATION_DEPOSIT
+  const Organization = await ethers.getContractFactory(ORGANIZATION_CONTRACT);
+  const organization = await Organization.deploy(ORGANIZATION_NAME, MEMBERS.slice(0, MEMBERS.length -1), { value: depositAmount});
 
   await organization.deployed();
   console.log("Organization deployed to:", organization.address);
+
+  // const filePath = `scripts/deployments/${HARDHAT_NETWORK}/organization.json`;
+  const filePath = ORGANIZATION_CONTRACT_FILE_PATH;
+  const fileData = {
+    organizationName: ORGANIZATION_NAME,
+    network: HARDHAT_NETWORK,
+    address: organization.address,
+    etherscan_url: `https://${HARDHAT_NETWORK}.etherscan.io/address/${organization.address}`,
+  }
+  writeJSONFile(filePath, fileData);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
