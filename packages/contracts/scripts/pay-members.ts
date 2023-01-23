0734d4c5d34b6a2321b066a393280808048c03c5
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Replaced hardcoded contract info with constants variables in paymembers script

--- packages/contracts/scripts/pay-members.ts
+++ packages/contracts/scripts/pay-members.ts
@@ -1,17 +1,20 @@
 import { ethers } from "hardhat";
 import { Contract } from "ethers";
-import { PAYROll } from "../utils/constants";
 
-// NOTE: This is the address of the deployed contract
-// NOTE: You can get this address from the console after running the deploy script
-// TODO: Add a way to get the address through a written file after running the deploy script.
-const organizationAddress = "0x3Af846E763872a0bC8B4977Be9Ee0aca957F8884";
-// NOTE: Be sure that members variable is are members of the organization 
-// else the transaction will fail.
+import { 
+  PAYROll, 
+  ORGANIZATION_CONTRACT, 
+  ORGANIZATION_DATA_JSON 
+} from "../utils/constants";
 
 async function main() {
     console.log("Paying members...");
-    const OrganizationContract = await ethers.getContractAt('Organization', organizationAddress);
+    const OrganizationContract = await ethers.getContractAt(
+      ORGANIZATION_CONTRACT, 
+      ORGANIZATION_DATA_JSON.address
+    );
+    // NOTE: Be sure that PAYROLL.MEMBERS variable are members of the organization 
+    // else the transaction will fail.
     const tx = await OrganizationContract.payMembers(PAYROll.MEMBERS, PAYROll.PAYMENTS);
     const txResult = await tx.wait();
     console.log('payMembers() txResult', txResult);
