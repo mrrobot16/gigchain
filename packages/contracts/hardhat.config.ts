1a9be92d82a184a9aae186cc3f8aa686e7b9f145
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Made changes to hardhat.config to have another account aside from the deployer one

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -8,6 +8,7 @@ dotenv.config()
 const { 
   INFURA_ID, 
   GOERLI_PRIVATE_KEY,
+  GOERLI_PRIVATE_KEY2, 
   GOERLI_INFURA_URL,
   MAINNET_PRIVATE_KEY,
   MAINNET_INFURA_URL,
@@ -17,6 +18,9 @@ const {
 const url = `${GOERLI_INFURA_URL}${INFURA_ID}`;
 
 const accounts = [`0x${GOERLI_PRIVATE_KEY}`];
+// NOTE: this private key is for the second account(accounts2) 
+// that is not the deployer to test the contract
+// const accounts = [`0x${GOERLI_PRIVATE_KEY2}`];
 
 const goerli = {
   url,
