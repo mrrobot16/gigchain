a4fbf012b7f4749586ef7d3c2c05a355a43ed438
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Replaced hardcoded contract info with constants variables in paymember script

--- packages/contracts/scripts/pay-member.ts
+++ packages/contracts/scripts/pay-member.ts
@@ -1,18 +1,21 @@
 import { ethers } from "hardhat";
 import { Contract, ContractInterface } from "ethers";
-import { MEMBERS } from "../utils/constants";
+import { 
+  MEMBERS, 
+  MEMBER,
+  PAY_MEMBER_AMOUNT,
+  ORGANIZATION_CONTRACT,
+  ORGANIZATION_DATA_JSON 
+} from "../utils/constants";
 
-// NOTE: This is the address of the deployed contract
-// NOTE: You can get this address from the console after running the deploy script
-// TODO: Add a way to get the address through a written file after running the deploy script.
-const organizationAddress = "0x3Af846E763872a0bC8B4977Be9Ee0aca957F8884";
-// NOTE: Be sure that member variable is a member of the organization else the transaction will fail.
-const member = MEMBERS[0];
-const amount = ethers.utils.parseEther("0.000001234567");
+// NOTE: Be sure that MEMBER variable is a member of the organization else the transaction will fail.
 async function main() {
     console.log("Paying member...");
-    const OrganizationContract = await ethers.getContractAt('Organization', organizationAddress);
-    const tx = await OrganizationContract.payMember(member, amount);
+    const OrganizationContract = await ethers.getContractAt(
+      ORGANIZATION_CONTRACT, 
+      ORGANIZATION_DATA_JSON.address
+    );
+    const tx = await OrganizationContract.payMember(MEMBER, PAY_MEMBER_AMOUNT);
     const txResult = await tx.wait();
     console.log('payMember() txResult', txResult);
 }
