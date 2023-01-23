9f2cd4d1181ccf427f5743abf355fbf61f84c10f
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Removed numbers utils since not needed for now

--- packages/contracts/utils/numbers.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from "hardhat";
-import { BigNumber } from "ethers";
-
-export function toNumber(number: BigNumber) {
-  const formattedEther = ethers.utils.formatEther(number);
-  return Number(formattedEther);
-}
