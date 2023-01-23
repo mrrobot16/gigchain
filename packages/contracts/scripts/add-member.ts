506ee6b40026a1ee2faffe676b377f4a9713bb3c
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Added script to add member to organization

---
+++ packages/contracts/scripts/add-member.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { 
+  NEW_MEMBER, 
+  ORGANIZATION_DATA_JSON, 
+  ORGANIZATION_CONTRACT 
+} from "../utils/constants";
+
+async function main() {
+    console.log("Adding member...");
+    const OrganizationContract = await ethers.getContractAt(
+      ORGANIZATION_CONTRACT, 
+      ORGANIZATION_DATA_JSON.address
+    );
+    const tx = await OrganizationContract.addMember(NEW_MEMBER);
+    const txResult = await tx.wait();
+    console.log('addMember() txResult', txResult);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
