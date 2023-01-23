1a24c293b28b0246c864ae7ac07070d53c61c913
hector <hmgarcia06@gmail.com>
2023-01-23 02:13:29 -0500

    Created modifiers and added pay members functions

--- packages/contracts/contracts/Organization.sol
+++ packages/contracts/contracts/Organization.sol
@@ -24,14 +24,12 @@ contract Organization {
         addMember(owner);
     }
 
-    function addMember(address _member) public {
-        require(msg.sender == owner, "Only the owner can add members");
+    function addMember(address _member) public onlyOwner("addMember") {
         members.push(_member);
         emit AddMember(_member);
     }
 
-    function removeMember(address _member) public {
-        require(msg.sender == owner, "Only the owner can remove members");
+    function removeMember(address _member) public onlyOwner("removeMember") {
         for (uint i = 0; i < members.length; i++) {
             if (members[i] == _member) {
                 members[i] = members[members.length - 1];
@@ -40,6 +38,35 @@ contract Organization {
                 break;
             }
         }
+    }
+
+    function payMember(address payable _member, uint256 _amount)
+        public payable
+        onlyOwner("payMember") 
+        correctAmount(_amount)
+        memberMustExist(_member)  
+        mustSendEther(_amount)
+    {
+        uint256 balance = address(this).balance;     
+        if(_member.send(_amount)) {
+           emit Transfer(address(this), _member, _amount);
+        } else {
+            revert TransferFailed();
+        }
+        balance = address(this).balance;
+    }
+
+    function payMembers(address[] memory _members, uint256[] memory _amounts) public onlyOwner("payMembers") {
+        // TODO: Need to refactor this to use array of objects with address and amount.
+        // TODO: Need to refactor this to use a mapping of address to amount.
+        require(_members.length == _amounts.length, "You must send the correct amount of members and amounts");
+        for (uint i = 0; i < _members.length; i++) {
+            address payable member = payable(_members[i]);
+            payMember(member, _amounts[i]);
+            emit Transfer(address(this), _members[i], _amounts[i]);
+        }
+    // TODO: Need to emit an event with the array of members and amounts.
+        emit MultiTransfer(address(this), _members, _amounts);
     }
 
     function getMembers() public view returns (address[] memory) {
@@ -59,33 +86,39 @@ contract Organization {
         return address(0);
     }
 
-    function payMember(address payable _member, uint256 _amount) public payable {
-        uint256 balance = address(this).balance;     
-        require(msg.sender == owner, "Only the owner can pay members");
-        require(_amount > 0, "You must send some Ether");
-        require(_amount <= balance, "You must send the correct amount of Ether");
-        require(getMember(_member) != address(0), "Member does not exist");
-        
-        if(_member.send(_amount)) {
-           emit Transfer(address(this), _member, _amount);
-        } else {
-            revert TransferFailed();
-        }
-        balance = address(this).balance;
-    }
-
-    function payMembers(address[] memory _members, uint256[] memory _amounts) public {
-        require(msg.sender == owner, "Only the owner can pay members");
-        require(_members.length == _amounts.length, "You must send the correct amount of members and amounts");
-        for (uint i = 0; i < _members.length; i++) {
-            address member = _members[i];
-            payMember(payable(member), _amounts[i]);
-            emit Transfer(address(this), _members[i], _amounts[i]);
-        }
-        emit MultiTransfer(address(this), _members, _amounts);
-    }
-
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
+
+    modifier onlyOwner(string memory functionName) {
+        string memory message = string(abi.encodePacked("Only the owner can call: ", functionName));
+        require(msg.sender == owner, message);
+        _;
+    }
+    
+    modifier memberMustExist(address _member) {
+        require(getMember(_member) != address(0), "Member does not exist");
+        _;
+    }
+
+    modifier mustSendEther(uint _amount) {
+        require(_amount > 0, "You must send some Ether");
+        _;
+    }
+
+    modifier correctAmount(uint _amount) {
+        uint256 balance = address(this).balance;
+        require(_amount <= balance, "You must send the correct amount of Ether");
+        _;
+    }
+
+    modifier correctAmounts(uint256[] memory _amounts) {
+        uint256 total = 0;
+        for (uint i = 0; i < _amounts.length; i++) {
+            total += _amounts[i];
+        }
+        require(total <= address(this).balance, "You must send the correct amount of Ether");
+        _;
+    }
+
 }
