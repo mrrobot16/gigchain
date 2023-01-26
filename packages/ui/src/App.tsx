0a32fb0094938233eb0875e95d903cea27cfa562
hector <hmgarcia06@gmail.com>
2023-01-25 23:59:32 -0500

    Added wagmi provider

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,14 +2,33 @@ import React from "react";
 import AppRouter from "router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { WagmiConfig  } from "wagmi"
+
+import { InjectedConnector } from '@wagmi/core/connectors/injected'
+import { createClient, configureChains, goerli } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { infuraProvider } from 'wagmi/providers/infura'
+
+const { INFURA_API_KEY } = process.env;
+const { chains, provider } = configureChains([goerli], [publicProvider(), infuraProvider({ apiKey: INFURA_API_KEY as string })])
+ 
+const client = createClient({
+  connectors: [
+    new InjectedConnector({ chains }),
+  ],
+  provider,
+})
+
 import { MUItheme } from "styles/theme";
 function App() {
   return (
     <>
       <CssBaseline />
-      <ThemeProvider theme={MUItheme}>
-        <AppRouter />
-      </ThemeProvider>
+        <WagmiConfig client={client}>
+          <ThemeProvider theme={MUItheme}>
+            <AppRouter />
+          </ThemeProvider>
+      </WagmiConfig>
     </>
   );
 }
