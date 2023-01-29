272967afca5689a85d79f237722a5c324d3174f6
hector <hmgarcia06@gmail.com>
2023-01-28 22:01:35 -0500

    Created dashboard members list in Organization page

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,7 +1,8 @@
 import CreateOrganizationForm from "components/CreateOrganizationForm";
 import { MemberList } from "components/members/MembersList";
-
+import { DashboardMemberList } from "components/members/DashboardMembersList";
 export { 
     CreateOrganizationForm,
     MemberList,
+    DashboardMemberList
 };
---
+++ packages/ui/src/components/members/DashboardMembersList.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { 
+  Typography,
+  List, 
+  ListItem, 
+  ListItemText, 
+  ListItemIcon,
+  TextField,
+} from "@mui/material";
+import { RemoveCircle as RemoveCircleIcon,  CurrencyBitcoin } from '@mui/icons-material';
+
+import { Member } from "types"
+
+interface DashboardMemberListProps {
+  members: Member[] | string[],
+  onRemoveMember: (member: string | number) => void
+  onPayMember: (member: string, amount: number) => void
+}
+
+export const DashboardMemberList = ({members, onRemoveMember, onPayMember}: DashboardMemberListProps) => {
+  // const [amount, setAmount] = React.useState(0);
+  return (
+    <List>
+      <Typography variant="h5" align="center">
+        List of Members
+      </Typography>
+      {
+        members.map((item, index) => {
+          const [amount, setAmount] = React.useState(0);
+          // item = typeof item == "string" ? item as string : item.address as string;
+          return (
+            <ListItem key={index}>
+              <ListItemText primary={typeof item == "string" ? item.slice(0,20) : item.address} />
+              <TextField inputProps={{ inputMode: 'numeric' }} type="number" onChange={(event)=>{
+                setAmount(parseInt(event.target.value));
+              }}/>
+              <ListItemIcon onClick={()=>{
+                // console.log('amount', amount);
+                
+                onPayMember(typeof item == "string" ? item : item.address, amount);
+              }}>
+                <CurrencyBitcoin/>
+              </ListItemIcon>
+
+              <ListItemIcon onClick={()=>{
+                onRemoveMember(typeof item == "string" ? item : item.address);
+              }}>
+                <RemoveCircleIcon/>
+              </ListItemIcon>
+            </ListItem>
+          )
+        })
+      }
+    </List>
+  )
+}
