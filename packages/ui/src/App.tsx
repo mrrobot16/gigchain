4657f8a9a5d91546fca727432dd4e129a99daefe
hector <hmgarcia06@gmail.com>
2023-01-31 17:57:53 -0500

    Removed wagmi provider and client component

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,33 +2,33 @@ import React from "react";
 import AppRouter from "router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import { WagmiConfig  } from "wagmi"
+// import { WagmiConfig  } from "wagmi"
 
-import { InjectedConnector } from '@wagmi/core/connectors/injected'
-import { createClient, configureChains, goerli } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
-import { infuraProvider } from 'wagmi/providers/infura'
+// import { InjectedConnector } from '@wagmi/core/connectors/injected'
+// import { createClient, configureChains, goerli } from 'wagmi'
+// import { publicProvider } from 'wagmi/providers/public'
+// import { infuraProvider } from 'wagmi/providers/infura'
 
-const { REACT_APP_INFURA_ID: INFURA_API_KEY } = process.env;
-const { chains, provider } = configureChains([goerli], [publicProvider(), infuraProvider({ apiKey: INFURA_API_KEY as string })])
+// const { REACT_APP_INFURA_ID: INFURA_API_KEY } = process.env;
+// const { chains, provider } = configureChains([goerli], [publicProvider(), infuraProvider({ apiKey: INFURA_API_KEY as string })])
  
-const client = createClient({
-  connectors: [
-    new InjectedConnector({ chains }),
-  ],
-  provider,
-})
+// const client = createClient({
+//   connectors: [
+//     new InjectedConnector({ chains }),
+//   ],
+//   provider,
+// })
 
 import { MUItheme } from "styles/theme";
 function App() {
   return (
     <>
       <CssBaseline />
-        <WagmiConfig client={client}>
+        {/* <WagmiConfig client={client}> */}
           <ThemeProvider theme={MUItheme}>
             <AppRouter />
           </ThemeProvider>
-      </WagmiConfig>
+      {/* </WagmiConfig> */}
     </>
   );
 }
