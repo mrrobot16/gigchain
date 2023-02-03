606582a8efadc692806ca4debf30335a73d4e1f1
hector <hmgarcia06@gmail.com>
2023-02-02 23:28:17 -0500

    Added function to utils that converts array of object with number to an array of numbers

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -3,3 +3,7 @@ import { Member } from 'types';
 export function convertToArrayOfAddresses(members: Member[]) {
   return members.map((member) => member.address);
 }
+
+export function convertToArrayOfAmounts(members: Member[]) {
+  return members.map((member: Member) => member.amount.toString());
+}
