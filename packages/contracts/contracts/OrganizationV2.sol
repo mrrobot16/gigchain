ba04f9814e10e65b999cb317071ef445bee7dc76
hector <hmgarcia06@gmail.com>
2023-02-06 05:11:37 -0500

    Contracts - fixed decoding for payMembersV2

--- packages/contracts/contracts/OrganizationV2.sol
+++ packages/contracts/contracts/OrganizationV2.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 struct Member {
     address account;
@@ -12,20 +12,19 @@ struct Member {
 struct Payment {
     address to;
     uint256 amount;
+    uint256 timestamp;
 }
 
 contract OrganizationV2 {
     address public owner;
     address public controller;
     string public name;
-    address[] public members;
     address[] public membersAccountsV2;
     uint256 public memberCount;
     mapping(address => Member) public membersV2;
-    // mapping(uint => Payment) public payments;
-    // Payment[] public memberPayments;
+    Payment[] public payments;
+
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event MultiTransfer(address indexed from, address[] indexed to, uint256[] value);
     event MultiTransferV2(address indexed from, address[] indexed to, uint256 value);
     event ReceiveEth(address indexed from, uint256 value);
     event AddMember(address indexed member);
@@ -99,55 +98,20 @@ contract OrganizationV2 {
         public
         onlyController("payMembersV2")
     {
-        (Payment[] memory payments) = abi.decode(_payments, (Payment[]));
-        uint256 balance = address(this).balance;
+        (bytes[] memory decoded) = abi.decode(_payments, (bytes[])); // This should a Payment[] struct instead of bytes[]. :/ 
         uint256 totalPayment = 0;
+        address[] memory payees = new address[](decoded.length);
 
-        address[] memory payees = new address[](payments.length);
-        for (uint i = 0; i < payments.length; i++) {
-            console.log("payments[i].to: %s", payments[i].to);
-            console.log("payments[i].amount: %s", payments[i].amount);
-            totalPayment += payments[i].amount;
-            payees[i] = payments[i].to;
+        for (uint i = 0; i < decoded.length; i++) {
+            (address to, uint256 amount) = abi.decode(decoded[i], (address, uint256));
+            totalPayment += amount;
+            if(address(this).balance < totalPayment) revert NotEnoughEtherBalance();
+            Payment memory payment = Payment(to, amount, block.timestamp);
+            payments.push(payment);
+            payees[i] = to;
+            payMemberV2(payable(to), amount);
         }
-        if(address(this).balance < totalPayment) {
-            revert NotEnoughEtherBalance();
-        }
-        for (uint i = 0; i < payments.length; i++) {
-            payMemberV2(payable(payments[i].to), payments[i].amount);
-        }
-        balance = address(this).balance;
-        console.log("After payMember Balance: %s :", balance);
         emit MultiTransferV2(address(this), payees, totalPayment);
-    }
-
-    function payMember(address payable _member, uint256 _amount)
-        public payable
-        onlyController("payMember") 
-        correctAmount(_amount)
-        memberMustExist(_member)  
-        mustSendEther(_amount)
-    {
-        uint256 balance = address(this).balance;     
-        if(_member.send(_amount)) {
-           emit Transfer(address(this), _member, _amount);
-        } else {
-            revert TransferFailed();
-        }
-        balance = address(this).balance;
-    }
-
-    function payMembers(address[] memory _members, uint256[] memory _amounts) public onlyController("payMembers") {
-        // TODO: Need to refactor this to use array of objects with address and amount.
-        // TODO: Need to refactor this to use a mapping of address to amount.
-        require(_members.length == _amounts.length, "You must send the correct amount of members and amounts");
-        for (uint i = 0; i < _members.length; i++) {
-            address payable member = payable(_members[i]);
-            payMember(member, _amounts[i]);
-            emit Transfer(address(this), _members[i], _amounts[i]);
-        }
-    // TODO: Need to emit an event with the array of members and amounts.
-        emit MultiTransfer(address(this), _members, _amounts);
     }
 
     function getMembersV2()
@@ -189,7 +153,6 @@ contract OrganizationV2 {
     }
     
     modifier mustExistMember(address _member) {
-        // console.log("mustExistMember _member: %s", _member);
         require(membersV2[_member].exists == true, "Member does not exist");
         _;
     }
