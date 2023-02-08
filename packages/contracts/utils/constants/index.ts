c5458f7f4b4d2cebeb2561c5c2c2144abf11f933
hector <hmgarcia06@gmail.com>
2023-02-07 20:28:18 -0500

    Contracts - Minor changes in constants utils

--- packages/contracts/utils/constants/index.ts
+++ packages/contracts/utils/constants/index.ts
@@ -18,9 +18,11 @@ export const ORGANIZATION_NAME = 'MyOrganization';
 export const ORGANIZATION_DEPOSIT = ethers.utils.parseEther('0.00016190');
 export const ORGANIZATION_DEPOSIT_TEST = ethers.utils.parseEther('10');
 export const ORGANIZATION_CONTRACT_FILE_PATH = `scripts/deployments/${Network}/organization.json`;
+export const ORGANIZATION_CONTRACT_FILE_PATH_V1 = `scripts/deployments/${Network}/organization-v1.json`;
 // NOTE: This is the path to the file where the organization address is stored
 const organizationFilePath = `../../${ORGANIZATION_CONTRACT_FILE_PATH}`;
 export const ORGANIZATION_DATA_JSON = require(`../../${ORGANIZATION_CONTRACT_FILE_PATH}`);
+export const ORGANIZATION_DATA_JSON_V1 = require(`../../${ORGANIZATION_CONTRACT_FILE_PATH_V1}`);
 
 const MEMBER_1 = '0x5Db06acd673531218B10430bA6dE9b69913Ad545';
 const MEMBER_1_PAYMENT = ethers.utils.parseEther('1');
