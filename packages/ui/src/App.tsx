69bfd251479df6debf6e1b29aebb96c56b29def3
hector <hmgarcia06@gmail.com>
2023-02-10 06:51:22 -0500

    UI - Added styles for dark mode. TODO: This styles should be part of the MaterialUI theme

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -2,34 +2,18 @@ import React from "react";
 import AppRouter from "router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-// import { WagmiConfig  } from "wagmi"
-
-// import { InjectedConnector } from '@wagmi/core/connectors/injected'
-// import { createClient, configureChains, goerli } from 'wagmi'
-// import { publicProvider } from 'wagmi/providers/public'
-// import { infuraProvider } from 'wagmi/providers/infura'
-
-// const { REACT_APP_INFURA_ID: INFURA_API_KEY } = process.env;
-// const { chains, provider } = configureChains([goerli], [publicProvider(), infuraProvider({ apiKey: INFURA_API_KEY as string })])
- 
-// const client = createClient({
-//   connectors: [
-//     new InjectedConnector({ chains }),
-//   ],
-//   provider,
-// })
 
 import { MUItheme } from "styles/theme";
 function App() {
   return (
-    <>
+    <div style={{backgroundColor: "#0d0208bd", color: "#003B00"}}>
       <CssBaseline />
         {/* <WagmiConfig client={client}> */}
           <ThemeProvider theme={MUItheme}>
             <AppRouter />
           </ThemeProvider>
       {/* </WagmiConfig> */}
-    </>
+    </div>
   );
 }
 
