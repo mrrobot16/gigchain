73ccdd66b909dce10ec6aab10f7e822c31f2ffe8
hector <hmgarcia06@gmail.com>
2023-02-12 16:03:28 -0500

    UI - Minor styling changes

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -5,18 +5,8 @@ import { ThemeProvider } from "@mui/material/styles";
 
 import { MUItheme } from "styles/theme";
 import { appStyles } from "styles/app";
-import { detectMetamask } from "utils/web3";
 
 function App() {
-  detectMetamask().then((hasMetamask: boolean) => {
-    if (!hasMetamask) {
-      console.log("Metamask is not installed or not available");
-      // alert("Install Meta Mask")
-      // window.open("https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en", "_blank");
-    } else {
-      console.log("Metamask is installed and available");
-    }
-  });
   return (
     <div style={appStyles}>
       <CssBaseline />
--- packages/ui/src/components/members/MembersList.tsx
+++ packages/ui/src/components/members/MembersList.tsx
@@ -13,13 +13,13 @@ import { Member } from "types"
 export const MemberList = ({members, onRemoveMember}:{ members: Member[], onRemoveMember: (index: number) => void }) => {
   return (
     <List>
-      <Typography variant="h5" align="center">
+      <Typography variant="h5">
         List of Members
       </Typography>
       {
         members.map((item, index) => (
-          <ListItem key={index}>
-              <ListItemText primary={(item as Member).address} />
+          <ListItem key={index} sx={{ paddingLeft: "unset", width: 250 }}>
+              <ListItemText primary={(item as Member).address.slice(0, 15) + "..."} />
               <ListItemIcon onClick={()=>{
                 onRemoveMember(index);
               }}>
