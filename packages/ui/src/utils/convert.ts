13f63b76434dc2178d1d3a2376aed375a94f9a26
hector <hmgarcia06@gmail.com>
2023-02-14 23:07:06 -0500

    UI - Moved convert utility functions to new file

---
+++ packages/ui/src/utils/convert.ts
@@ -0,0 +1,25 @@
+import { Member } from 'types';
+
+export function convertToArrayOfAddresses(members: Member[]) {
+  return members.map((member) => member.address);
+}
+
+export function convertToArrayOfAmounts(members: Member[]) {
+  return members.map((member: Member) => member.amount.toString());
+}
+
+export function convertMembersArrayToArrayOfObject(members: []) {
+  const membersArray: Member [] = members.map((membersArr: []) => {
+    const tempArr: (string | number | boolean)[] = []
+    membersArr.forEach((member) => {
+      tempArr.push(member);
+    });
+    return {
+      address: tempArr[0] as string,
+      amount: tempArr[1] as number,
+      exists: tempArr[2] as boolean,
+      active: tempArr[3] as boolean,
+    }
+  });
+  return membersArray
+}
