0617b8d301f1b178b02196d6861e26675dc496f1
hector <hmgarcia06@gmail.com>
2023-02-16 22:21:48 -0500

    UI - minor ui test change

--- packages/ui/src/tests/App.test.tsx
+++ packages/ui/src/tests/App.test.tsx
@@ -4,6 +4,6 @@ import App from "App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to Gigchain/i);
+  const welcomeElement = screen.getByText(/Create an Organization/i);
   expect(welcomeElement).toBeInTheDocument();
 });
