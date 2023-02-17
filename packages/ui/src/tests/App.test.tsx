8577dd8578c49a5a0d1953d9cb3bdfa937cb1ce2
hector <hmgarcia06@gmail.com>
2023-02-17 18:15:44 -0500

    UI - added test for organization container, currently failed because navigation to Organization container is missing params

--- packages/ui/src/tests/App.test.tsx
+++ packages/ui/src/tests/App.test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "App";
+import { Organization } from "containers";
 
 test("renders learn react link", () => {
   render(<App />);
   const welcomeElement = screen.getByText(/Create an Organization/i);
   expect(welcomeElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<Organization />);
+  // const organizationName = screen.getByText(/Organization name/i);
+  // expect(organizationName).toBeInTheDocument();
+  // const organizationAddress = screen.getByText(/Organization Address/i);
+  // expect(organizationAddress).toBeInTheDocument();
+  // const organizationBalance = screen.getByText(/Organization Balance/i);
+  // expect(organizationBalance).toBeInTheDocument();
 });
